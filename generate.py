4b5bff56053f8e31bed38e6ce1b8298ff2cc635d
Harsh Shandilya <msfjarvis@gmail.com>
2016-11-19 12:29:37 +0530

    crappy shit

--- generate.py
+++ generate.py
@@ -8,16 +8,26 @@ PAGE_COUNT = 9
 def dump_devices(d,f):
   json.dump(f,d,indent=2)
 
-_f=open('devices.json','w')
+#_f=open('devices.json','w')
 list_devices=[]
-i=1
 
-while i <= 9:
-  print "pass {0} of {1}".format(str(i),str(PAGE_COUNT))
-  payload={'action':'devices','limit':'100','page':i}
-  r = requests.get(AFH_API_ENDPOINT, params=payload)
-  list_devices.extend(r.json()['DATA'])
-  print "Currently synced down {0} devices!".format(str(len(list_devices)))
-  i+=1
+def get_devices():
+  i = 0
+  while i <= 9:
+    print "pass {0} of {1}".format(str(i),str(PAGE_COUNT))
+    payload={'action':'devices','limit':'100','page':i}
+    r = requests.get(AFH_API_ENDPOINT, params=payload)
+    list_devices.extend(r.json()['DATA'])
+    print "Currently synced down {0} devices!".format(str(len(list_devices)))
+    i+=1
 
-json.dump(list_devices,_f,indent=2)
+def recurse_devices_for_files():
+  j = 0
+  for i in list_devices:
+    print "recursing for {0}".format(i["device_name"])
+    payload={'action':'developers', 'did':i["did"]}
+    r = requests.get(AFH_API_ENDPOINT, payload)
+    print r.json()
+
+list_devices=json.load(open('devices.json','r'))
+recurse_devices_for_files()
