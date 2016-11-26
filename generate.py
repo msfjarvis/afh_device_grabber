b3e2d50328dcac7d591ceff404056f931bd29922
Harsh Shandilya <msfjarvis@gmail.com>
2016-11-26 18:15:06 +0530

    For each device, also sync down developers

--- generate.py
+++ generate.py
@@ -3,10 +3,6 @@ import json
 import requests
 
 AFH_API_ENDPOINT = "https://androidfilehost.com/api/"
-PAGE_COUNT = 0 # we'll calculate this below, chill
-
-def dump_devices(d,f):
-  json.dump(f,d,indent=2)
 
 _f=open('devices.json','w')
 list_devices=[]
@@ -17,6 +13,18 @@ def count_pages():
     temp_page_count = int(data['TOTALS']['total_objects'])
     global PAGE_COUNT
     PAGE_COUNT = (temp_page_count/100) + 1
+
+def fetch_devs(did):
+    devs = []
+    payload={'action':'developers','page':'1','limit':'1','did':did}
+    data=requests.get(AFH_API_ENDPOINT, params=payload).json()
+    temp_dev_pages_count = (int(data['TOTALS']['total_objects'])/100) + 1
+    while i <= temp_dev_pages_count:
+        print "pass {0} of {1}".format(str(i),str(temp_dev_pages_count))
+        payload={'action':'developers','page':i,'limit':'100','did':did}
+        devs.extend(requests.get(AFH_API_ENDPOINT,params=payload)).json()['DATA']
+    return devs
+
 
 def get_devices():
     i = 1
@@ -29,5 +37,12 @@ def get_devices():
       i+=1
     json.dump(list_devices,_f,indent=2)
 
+def get_developers():
+    for i in list_devices:
+        devs = fetch_devs(i['did'])
+        _f = open(i['device_name'], 'w')
+        json.dump(devs,_f,indent=2)
+
 count_pages()
 get_devices()
+get_developers()
