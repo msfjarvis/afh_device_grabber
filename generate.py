05dc2513827fc21325e9b954d1eacc67b2add167
Ritayan Chakraborty <ritayanout@gmail.com>
2017-12-16 12:51:33 +0530

    Fix fetching devices and add retries there, too

--- generate.py
+++ generate.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import json
-import requests
 import cfscrape
 import time
 import os
@@ -46,14 +45,70 @@ def count_pages():
 def fetch_devs(did):
     scraper = cfscrape.create_scraper()
     devs = []
+    retries = 0
     payload={'action':'developers','page':'1','limit':'1','did':did}
-    data=scraper.get(AFH_API_ENDPOINT, params=payload).json()
-    temp_dev_pages_count = (int(data['TOTALS']['total_objects'])/100) + 1
+    while True:
+        raw = scraper.get(AFH_API_ENDPOINT, params=payload)
+        objects = 0
+        try:
+            objects = int(raw.json()['TOTALS']['total_objects'])
+        except ValueError:
+            if retries < MAX_RETRIES:
+                retries += 1
+                msg = "Error fetching number of pages of developers for device "
+                msg += str(did)
+                print raw.content
+                print msg
+                time.sleep(5)
+                # Retrying. So far, this has been a HTTP 502, and a retry fixes it
+                # TODO: Check if it really is a 502
+                continue
+            else:
+                errors += "\nError: could not fetch number of pages of developers for device "
+                errors += str(did)
+                return devs
+        except KeyError:
+            errors += "\nError: invalid JSON on pages of developers for device "
+            errors += str(did)
+            return devs
+        except TypeError:
+            # No developers
+            return devs
+        break
+
+    temp_dev_pages_count = (objects / 100) + 1
+    retries = 0
+    i = 1
     while i <= temp_dev_pages_count:
-        print "pass {0} of {1}".format(str(i),str(temp_dev_pages_count))
+        print "pass {0} of {1}".format(str(i), str(temp_dev_pages_count))
         payload={'action':'developers','page':i,'limit':'100','did':did}
-        devs.extend(scraper.get(AFH_API_ENDPOINT,params=payload)).json()['DATA']
+        data = []
+        raw = scraper.get(AFH_API_ENDPOINT,params=payload)
+        try:
+            data = raw.json()['DATA']
+        except ValueError:
+            if retries < MAX_RETRIES:
+                retries += 1
+                print raw.content
+                print "Error fetching page {0} of developers for device {1}".format(str(i), str(did))
+                time.sleep(5)
+                # Retrying. So far, this has been a HTTP 502, and a retry fixes it
+                # TODO: Check if it really is a 502
+                continue
+            else:
+                errors += "\nError: could not fetch page {0} of developers for device {1}".format(str(i), str(did))
+                retries = 0
+                i += 1
+                continue
+        except KeyError:
+            errors += "\nError: invalid JSON on page {0} of developers for device {1}".format(str(i), str(did))
+            retries = 0
+            i += 1
+            continue
+        devs.extend(data)
         time.sleep(3)
+        retries = 0
+        i += 1
     return devs
 
 
@@ -72,23 +127,19 @@ def get_devices():
       except ValueError:
           if retries < MAX_RETRIES:
               retries += 1
-              msg = "Error on page "
-              msg += str(i)
               print r.content
-              print msg
+              print "Error fetching page {0} of devices".format(str(i))
               time.sleep(5)
               # Retrying. So far, this has been a HTTP 502, and a retry fixes it
               # TODO: Check if it really is a 502
               continue
           else:
-              errors += "\nError: could not fetch page "
-              errors += str(i)
+              errors += "\nError: could not fetch page {0} of devices".format(str(i))
               retries = 0
               i += 1
               continue
       except KeyError:
-          errors += "\nError: invalid JSON on page "
-          errors += str(i)
+          errors += "\nError: invalid JSON on page {0} of devices".format(str(i))
           retries = 0
           i += 1
           continue
@@ -100,8 +151,12 @@ def get_devices():
 
 def get_developers():
     for i in list_devices:
+        # Skipping devices we know has a huge number of devs. Trying to avoid DoS-ing here.
+        if i['did'] == "51" or i['did'] == "395":
+            continue
+        print "Fetching developers for device {0} {1}".format(i['manufacturer'], i['device_name'])
         devs = fetch_devs(i['did'])
-        _f = open(i['device_name'], 'w')
+        _f = open(i['did'], 'w')
         json.dump(devs,_f,indent=2)
 
 count_pages()
