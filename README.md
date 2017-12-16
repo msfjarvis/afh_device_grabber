02037c802cea87e84431548d3f143e72cbb5f4c3
Ritayan Chakraborty <ritayanout@gmail.com>
2017-12-16 08:22:20 +0530

    Use cloudflare-scrape to make this work again

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### afh_device_grabber
 
 Grabs a list of devices from [AndroidFileHost.com](https://androidfilehost.com) using their API
+Install cloudflare-scrape (https://github.com/Anorov/cloudflare-scrape) with "pip install cfscrape"
--- generate.py
+++ generate.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import json
 import requests
+import cfscrape
 
 AFH_API_ENDPOINT = "https://androidfilehost.com/api/"
 
@@ -8,30 +9,33 @@ _f=open('devices.json','w')
 list_devices=[]
 
 def count_pages():
+    scraper = cfscrape.create_scraper()
     payload={'action':'devices','limit':'1'}
-    data = requests.get(AFH_API_ENDPOINT, params=payload).json()
+    data = scraper.get(AFH_API_ENDPOINT, params=payload).json()
     temp_page_count = int(data['TOTALS']['total_objects'])
     global PAGE_COUNT
     PAGE_COUNT = (temp_page_count/100) + 1
 
 def fetch_devs(did):
+    scraper = cfscrape.create_scraper()
     devs = []
     payload={'action':'developers','page':'1','limit':'1','did':did}
-    data=requests.get(AFH_API_ENDPOINT, params=payload).json()
+    data=scraper.get(AFH_API_ENDPOINT, params=payload).json()
     temp_dev_pages_count = (int(data['TOTALS']['total_objects'])/100) + 1
     while i <= temp_dev_pages_count:
         print "pass {0} of {1}".format(str(i),str(temp_dev_pages_count))
         payload={'action':'developers','page':i,'limit':'100','did':did}
-        devs.extend(requests.get(AFH_API_ENDPOINT,params=payload)).json()['DATA']
+        devs.extend(scraper.get(AFH_API_ENDPOINT,params=payload)).json()['DATA']
     return devs
 
 
 def get_devices():
+    scraper = cfscrape.create_scraper()
     i = 1
     while i <= PAGE_COUNT:
       print "pass {0} of {1}".format(str(i),str(PAGE_COUNT))
       payload={'action':'devices','limit':'100','page':i}
-      r = requests.get(AFH_API_ENDPOINT, params=payload)
+      r = scraper.get(AFH_API_ENDPOINT, params=payload)
       list_devices.extend(r.json()['DATA'])
       print "Currently synced down {0} devices!".format(str(len(list_devices)))
       i+=1
